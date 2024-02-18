e9ddb4e7cc9bb515acb34c5888fad3c0dfc13130
Nilu patel <nilusingh945164@gmail.com>
2024-02-18 16:55:00 +0530

    g

---
+++ Generice.java/generic.java
@@ -0,0 +1,11 @@
+//package Generice.java;
+
+import java.util.ArrayList;
+
+public class generic {
+   public static void main(String[] args) {
+    
+    ArrayList a=new ArrayList();
+    
+   } 
+}
