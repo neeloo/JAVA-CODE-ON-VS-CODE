62d161d97f1e00f9aa081b0cedaefd9cc7d4d7b6
Nilu patel <nilusingh945164@gmail.com>
2024-02-20 17:29:26 +0530

    j

--- Generice.java/Generice/java/generic.java
+++ Generice.java/Generice/java/generic.java
@@ -14,8 +14,33 @@ public class generic {
       a.add(78);
       int n=(int)a.get(2);
       System.out.println(a);
-
-
     
    } 
+}
+class myj<T1>{
+int val;
+private T1 t1;
+
+public myj(int val,T1 t1){
+   this.val=val;
+   this.t1=t1;
+
+}
+int getVal(){
+   return val;
+}
+
+void setVal(int val){
+this.val=val;
+}
+
+T1 getT1(){
+   return t1;
+}
+
+void setT1(T1 t1){
+   this.t1=t1;
+}
+
+
 }
