93506ee991f0afd7e93d82d8a51a95c37ef5d51e
Nilu patel <nilusingh945164@gmail.com>
2024-02-20 17:14:28 +0530

    j

--- Generice.java/Generice/java/generic.java
+++ Generice.java/Generice/java/generic.java
@@ -10,6 +10,10 @@ public class generic {
    ArrayList a=new ArrayList();
 
       a.add("str1");
+      a.add(90);
+      a.add(78);
+      int n=(int)a.get(2);
+      System.out.println(a);
 
 
     
