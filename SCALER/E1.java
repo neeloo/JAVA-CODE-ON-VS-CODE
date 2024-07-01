4facba8004ef99171cf0ae8ecd7d2904b7e3ba1f
Nilu patel <nilusingh945164@gmail.com>
2024-07-01 12:08:02 +0530

    java

---
+++ SCALER/E1.java
@@ -0,0 +1,15 @@
+public class E1 {
+    public static void main(String[] args) {
+         int arr[] ={1,2,34,3,4,5,7,23,12};
+        System.out.println( threeConsecutiveOdds(arr));
+
+    }
+    public static boolean threeConsecutiveOdds(int[] arr) {
+        StringBuilder binaryString = new StringBuilder();
+        for (int num : arr) {
+            binaryString.append(num % 2);
+        }
+        return binaryString.toString().contains("111");
+    }
+    
+}
